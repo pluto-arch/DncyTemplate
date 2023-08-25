77aa97c0a7637abe590f0692fe0e53a1bf916946
张玉龙 <620524a2924.cdb@sina.cn>
2023-08-25 22:47:42 +0800

    moq安全性问题暂时去除此包

--- template/test/DncyTemplate.UnitTest/DemoTest.cs
+++ template/test/DncyTemplate.UnitTest/DemoTest.cs
@@ -8,7 +8,6 @@ using DncyTemplate.Application.Models.Product;
 using DncyTemplate.Domain.Aggregates.Product;
 using DncyTemplate.Domain.Infra.Repository;
 using DncyTemplate.Infra.EntityFrameworkCore.Repository;
-using Moq;
 using DncyTemplate.Domain.Infra;
 
 namespace DncyTemplate.UnitTest
@@ -37,38 +36,6 @@ namespace DncyTemplate.UnitTest
 #if Tenant
             }
 #endif
-        }
-
-
-        [Test]
-        public async Task MoqTest()
-        {
-            var fakeProducts = new Faker<Product>()
-                .RuleFor(x => x.Id, s => s.Random.Guid().ToString("N"))
-                .RuleFor(x => x.Name, f => f.Lorem.Word())
-                .RuleFor(x => x.CreationTime, f => f.Date.Soon())
-                .Generate(1000);
-
-
-            var first = fakeProducts.First();
-            var productQuerable = fakeProducts.AsQueryable();
-
-
-            var mockAsyncExecuter = new Mock<IAsyncQueryableProvider>();
-            mockAsyncExecuter.Setup(x => x.FirstOrDefaultAsync(It.IsAny<IQueryable<Product>>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(first);
-
-
-            var mockRepository = new Mock<IEfRepository<Product, string>>();
-            mockRepository.Setup(r => r.AsyncExecuter).Returns(mockAsyncExecuter.Object);
-            mockRepository.Setup(r => r.QuerySet).Returns(productQuerable);
-
-
-
-            var result = await mockRepository.Object.FirstOrDefaultAsync();
-            Assert.That(result, Is.Not.Null);
-
-            Assert.That(first.Name, Is.EqualTo(result.Name));
         }
 
     }
