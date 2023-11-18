a5855431d11f8ae5220b8d8c8d4ebb893b45be6d
yulong <1976935046@qq.com>
2023-11-19 00:52:34 +0800

    aspire 条件符号

--- template/AppHost/DncyTemplate.AppHost/Program.cs
+++ template/AppHost/DncyTemplate.AppHost/Program.cs
@@ -1,12 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 
-#if (ProjectType == "API")
+#if IsAPI
 builder.AddProject<Projects.DncyTemplate_Api>("dncytemplate.api");
 #endif
 
-#if (ProjectType == "MVC")
+#if IsMVC
 builder.AddProject<Projects.DncyTemplate_Mvc>("dncytemplate.mvc");
 #endif
+
 
 builder.Build().Run();
