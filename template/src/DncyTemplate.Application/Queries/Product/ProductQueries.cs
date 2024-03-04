29f9077ae19398643e220e2b058e0695d0b2a3eb
yulong <620524a2924.cdb@sina.cn>
2024-03-04 21:53:56 +0800

    修复条件编译tenant问题

--- template/src/DncyTemplate.Application/Queries/Product/ProductQueries.cs
+++ template/src/DncyTemplate.Application/Queries/Product/ProductQueries.cs
@@ -1,12 +1,13 @@
 ﻿#if Tenant
 using Dotnetydd.MultiTenancy;
+using Dotnetydd.MultiTenancy.ConnectionStrings;
 #endif
 using DncyTemplate.Constants;
 using DncyTemplate.Application.AppServices.Queries.ConnectionFactory;
 using DncyTemplate.Application.Models.Product;
 using System.Data;
 using DncyTemplate.Domain.Infra;
-using Dotnetydd.MultiTenancy.ConnectionStrings;
+
 
 namespace DncyTemplate.Application.Queries.Product
 {
